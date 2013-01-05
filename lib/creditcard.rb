97b7edca0488199a17581600ec9a33215bec74f4
Tony To <tonyto85@gmail.com>
2013-01-05 15:39:43 +0000

    identified AMEX card type

--- lib/creditcard.rb
+++ lib/creditcard.rb
@@ -1,8 +1,13 @@
 class CreditCard
+  attr_accessor :number
+
   def initialize number
+    @number = number
   end
 
   def identify
+    return "AMEX" if number.start_with?('34')
+    nil
   end
 
 end
